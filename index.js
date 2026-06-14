1dcac0f5e4b939915f9e9bb6d99516f574326757
Hawa-codes <adediranhawa@gmail.com>
2026-06-14 20:29:36 +0100

    Express Intro

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { error } = require("node:console");
 
 const app = express();
+app.use(express.json());
 
 const users = [
   { id: 1, name: "Hawa" },
@@ -25,11 +26,10 @@ app.get("/users", (req, res) => {
     res.status(200).json({users})
 })
 
+// http://localhost:4000/users/2
 app.get("/users/:id", (req, res) => {
     const id = req.params.id;
-    
-    const user = users.find((u) => u.id == id);
-
+    const user = users.find((u) => u.id === id);
     if (!user) {
     return res.status(404).json({
       message: "user not found",
@@ -41,7 +41,7 @@ app.get("/users/:id", (req, res) => {
 
 app.post("/user", (req, res) => {
     const { name } = req.body;
-    const user = { id: Math.random(), name};
+    const user = { id: Math.floor(Math.random() * 10), name};
     users.push(user);
     res.status(201).json({
         message: "user created successfully",
@@ -49,7 +49,7 @@ app.post("/user", (req, res) => {
     });
 });
 
-app.patch("/user/:id", (req, res) => {
+app.patch("/users/:id", (req, res) => {
     const { body, params } = req;
     const email = body.email
     if (!email) {
@@ -73,17 +73,68 @@ app.patch("/user/:id", (req, res) => {
   });
 });
 
-app.put("/user/:id", (req, res) => {
-    const { body, params } = req
-    const { name, email, age } = body;
-
-  if (!name || !email || !age) {
-    return res.status(422).json({
-      message: "All fields are required",
+app.put("/users/:id", (req,res) => {
+    const {body, params} = req;
+    const { name, email, age } =  body;
+    if (!name || !email || !age) {
+        return res.status(404).json({
+            message: "All fields are required",
+            error: true,
+        });
+    }
+    const userIndex = users.findIndex(({ id }) => id == params.id);
+    if (userIndex === -1) {
+    return res.status(404).json({
+      message: "user not found",
       error: true,
     });
   }
+    users[userIndex] = {
+        id: users[userIndex].id,
+        name,
+        email,
+        age,
+    };
+
+    res.status(200).json({
+    message: "user updated successfully",
+    data: users[userIndex],
+  });
 })
+
+app.delete("/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((u) => u.id === id);
+    if (userIndex === -1) {
+        return res.status(404).json({
+        message: "user not found",
+        error: true,
+        });
+    }
+
+    const deletedUser = users.splice(userIndex, 1);
+    return res.status(200).json({
+        message: "user deleted successfully",
+        data: deletedUser[0],
+    });
+})
+
+// app.put("/user/:id", (req, res) => {
+//     const { body, params } = req
+//     const { name, email, age } = body;
+
+//   if (!name || !email || !age) {
+//     return res.status(422).json({
+//       message: "All fields are required",
+//       error: true,
+//     });
+//   }
+// })
+
+
+
+
+
 
 // How do you create another route called /products ?
 app.get("/products", (req, res) => {
@@ -108,17 +159,4 @@ app.get("/products/:id", (req, res) => {
 app.listen(4003, () => {
   console.log("Server is running on port 4000");
 });
-
-/* 
-Express Basics
-What is Express.js?
-What does express() do?
-What is the difference between Node.js and Express.js?
-What is a route?
-Routes
-What does .get() mean?
-What is the difference between / and /users?
-How do you create another route called /about?
-What happens if a route doesn't exist?
-*/
 
